1338b136f81eafd5e88aced2a8a564a5f185942d
PedroH183 <pedro_freires18@outlook.com>
2025-06-29 15:53:39 -0300

    feat: add metrics tracking and endpoints for cleaning and retrieving metrics

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from contextlib import asynccontextmanager
+import time
+from typing import List
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.responses import JSONResponse
 
@@ -17,6 +19,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+METRICS: List[float] = []
 
 
 def get_conn():
@@ -25,11 +28,32 @@ def get_conn():
 
 @app.get("/clientes/{cliente_id}")
 async def ler_cliente(cliente_id: int, conn=Depends(get_conn)):
+    
+    start_time = time.perf_counter()
     row = await conn.fetchrow(
         "SELECT id, nome, email FROM clientes WHERE id = $1", cliente_id
     )
+    end_time = time.perf_counter()
+    
+    global METRICS
+    METRICS.append(end_time - start_time)
+
     if not row:
         raise HTTPException(status_code=404, detail="Cliente não encontrado")
 
     row = dict(row)
     return JSONResponse(row)
+
+
+@app.get("/metrics")
+async def metrics():
+    global METRICS
+    return JSONResponse({"status": "ok", "message": METRICS})
+
+
+@app.put("/clean-metrics")
+async def clean_metrics():
+
+    global METRICS
+    METRICS.clear()
+    return JSONResponse({"status": "ok", "message": "Metrics cleaned."})
